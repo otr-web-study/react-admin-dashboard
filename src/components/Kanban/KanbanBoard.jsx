ba96f6ca32840d4ed7e1225f78812949346d2e0e
mint <ge10rik@gmail.com>
2023-10-07 12:54:11 +0700

    again kanban

--- src/components/Kanban/KanbanBoard.jsx
+++ src/components/Kanban/KanbanBoard.jsx
@@ -1,35 +1,74 @@
 import { useState, useLayoutEffect } from 'react';
-import { DndContext } from '@dnd-kit/core';
-import { SortableContext } from '@dnd-kit/sortable';
+import { createPortal } from 'react-dom';
+import { DndContext, DragOverlay } from '@dnd-kit/core';
+import { SortableContext, arrayMove } from '@dnd-kit/sortable';
 import KanbanColumn from './KanbanColumn';
 
-const KanbanBoard = ({ columns, items, keyField }) => {
+const KanbanBoard = ({ columns: initialColumns, items, keyField }) => {
   const [tasks, setTasks] = useState({});
   const [columnIds, setColumnIds] = useState([]);
+  const [activeColumn, setActiveColumn] = useState(null);
+  const [columns, setColumns] = useState(initialColumns);
+
+  const onDragStart = (evt) => {
+    if (evt.active.data.current?.type === 'column') {
+      setActiveColumn(evt.active.data.current.column);
+      return;
+    }
+  };
+
+  const onDragEnd = (evt) => {
+    const { active, over } = evt;
+
+    if (!over) return;
+
+    const activeColId = active.id;
+    const overColId = over.id;
+
+    if (activeColId === overColId) return;
+
+    setColumns((columns) =>
+      arrayMove(
+        columns,
+        columns.findIndex((c) => c.keyField === activeColId),
+        columns.findIndex((c) => c.keyField === overColId),
+      ),
+    );
+  };
 
   useLayoutEffect(() => {
     setTasks(
-      columns.reduce((acc, col) => {
+      initialColumns.reduce((acc, col) => {
         const colTasks = items.filter((i) => i[keyField] === col.keyField);
         acc[col.keyField] = colTasks || [];
 
         return acc;
       }, {}),
     );
-  }, [columns, items, keyField]);
+  }, [initialColumns, items, keyField]);
 
   useLayoutEffect(() => {
     setColumnIds(columns.map((c) => c.keyField));
   }, [columns]);
 
-  const content = columns.map((col) => (
-    <KanbanColumn key={col.keyField} column={col} tasks={tasks[col.keyField]} />
-  ));
+  const content = columns
+    ? columns.map((col) => (
+        <KanbanColumn key={col.keyField} column={col} tasks={tasks[col.keyField]} />
+      ))
+    : null;
 
   return (
     <div className="flex overflow-x-auto overflow-y-hidden gap-4 justify-stretch">
-      <DndContext>
+      <DndContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
         <SortableContext items={columnIds}>{content}</SortableContext>
+        {createPortal(
+          <DragOverlay>
+            {activeColumn && (
+              <KanbanColumn column={activeColumn} tasks={tasks[activeColumn.keyField]} />
+            )}
+          </DragOverlay>,
+          document.body,
+        )}
       </DndContext>
     </div>
   );
--- src/components/Kanban/KanbanColumn.jsx
+++ src/components/Kanban/KanbanColumn.jsx
@@ -1,15 +1,38 @@
 import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import KanbanTask from './KanbanTask';
 
 const KanbanColumn = ({ column, tasks }) => {
+  const { setNodeRef, attributes, listeners, transform, transition, isDragging } = useSortable({
+    id: column.keyField,
+    data: {
+      type: 'column',
+      column,
+    },
+  });
+
+  const style = {
+    transition,
+    transform: CSS.Transform.toString(transform),
+  };
+
+  if (isDragging) {
+    style['opacity'] = 0.4;
+  }
+
   const content = tasks ? tasks.map((t) => <KanbanTask key={t.Id} task={t} />) : null;
+
   return (
-    <div className="flex-1 min-w-[150px] bg-light-gray rounded p-3 min-h-[500px] flex flex-col max-w-[350px]">
-      <h3 className="font-medium">
+    <div
+      ref={setNodeRef}
+      style={style}
+      className="flex-1 min-w-[150px] bg-light-gray rounded min-h-[500px] pb-3 flex flex-col max-w-[350px] shadow-md"
+    >
+      <h3 className="font-medium p-3" {...attributes} {...listeners}>
         {column.headerText}
         <span className="font-normal ml-1">{`(${tasks && tasks.length})`}</span>
       </h3>
-      <div className="my-4 flex flex-col gap-3">{content}</div>
+      <div className="my-4 flex flex-col gap-3 mx-3">{content}</div>
     </div>
   );
 };
